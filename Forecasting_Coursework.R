161d8fea6b2feb949139addfe8d9b573ed5f157e
LauraWiltshire <119063277+LauraWiltshire@users.noreply.github.com>
2023-01-14 19:56:18 +0000

    Part 1 description.

--- Forecasting_Coursework.R
+++ Forecasting_Coursework.R
@@ -1,2 +1,14 @@
 # Welcome to my Forecasting Coursework
-# Second working commit
+################################################################################
+# Part 1: Manual Modelling
+# This uses quaterly time series data from the M3 competition (package Mcomp)
+# My student ID ends in a 3, so the Series ID I will use is 1357.
+# I shall manually select three models, for fitting data and producing forecasts.
+# One regression model (with trend and seasonality components)
+# One exponential smoothing model
+# One ARIMA model
+# I will perform data exploration using graphs, 
+# statistical descriptive summaries,
+# and statistical tests.
+# This includes residual diagnostics, prediction intervals (80% and 95% CI)
+# for the next 8 quaters
